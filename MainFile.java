bbb5a7a1629b3109e42a4e3d5b14e4d64358adee
xmr-p <223111383@upmh.edu.mx>
2024-08-05 15:23:40 -0600

    Small corrections

--- MainFile.java
+++ MainFile.java
@@ -1,7 +1,7 @@
 public class MainFile {
     public static void main(String[] args) {
         System.out.println("Main!");
-        Kingdom kingdom1 = new Kingdom("K1", "C://path/to/file.png", "Norte de algo", "coin1", new String[]{""}, new String[]{"K2", "K3"});
+        Kingdom kingdom1 = new Kingdom("Kingdom of ITI", "C://path/to/file.png", "North Hemisphere", "ITIcoin", new String[]{""}, new String[]{"K2", "K3"});
         King king1 = new King("Mr.", "Rey", "Reyes", true, true, false, false, true, 100.0f, 1000.5f, 0.15f, 28, kingdom1, Occupation.KING, HealthState.HEALTHY, false);
         king1.makeDecree("Decree1");
         king1.makeDecree("Decree2");
@@ -12,9 +12,9 @@ public class MainFile {
             //Punch
         Weapon flail = new Weapon("punch",2.3f,Occupation.VILLAGER);
         Weapon morningStar = new Weapon("punch",2.8f,Occupation.VILLAGER);
-            //Bury
-        Weapon spear = new Weapon("bury",1.8f,Occupation.NOBLE);
-        Weapon pike = new Weapon("bury",3.4f,Occupation.NOBLE);
+            //Stab
+        Weapon spear = new Weapon("stab",1.8f,Occupation.NOBLE);
+        Weapon pike = new Weapon("stab",3.4f,Occupation.NOBLE);
         //Soldier
         Soldier royalGuard1 = new Soldier("Royal Guard","vanguard",new String[]{"patrol","guard","escort","train","report"},new Weapon[]{spear});
         Soldier royalGuard2 = new Soldier("Royal Guard","vanguard",new String[]{"protect King","ceremonial duties","enforce orders","escort VIPs","guard castle"},new Weapon[]{pike});
