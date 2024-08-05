25019595caf194bec83eea1304978805c1ca7128
Alfonso Morales <223110550@upmh.edu.mx>
2024-08-05 15:19:40 -0600

    main draft

--- Villager.java
+++ Villager.java
@@ -5,10 +5,16 @@ public class Villager extends LowerClass{
     Tool[] tools;
 
     public Villager(boolean bow, UpperClass boss, Soldier soldierType, Tool[] tools, boolean isSoldier, String firstname, String paternalSurname, String maternalSurname, boolean gender, boolean royalBlood, boolean darknessInside, boolean banished, boolean hasEducation, float wage, float fortune, float taxes, int age, Kingdom kingdom, Occupation occupation, HealthState healthState) {
-        super(firstname, paternalSurname, maternalSurname, gender, royalBlood, darknessInside, banished, hasEducation, wage, fortune, taxes, age, kingdom, occupation, healthState,bow);
+        super(firstname, paternalSurname, maternalSurname, gender, royalBlood, darknessInside, banished, hasEducation, wage, fortune, taxes, age, kingdom, occupation, healthState, bow);
         this.boss = boss;
         this.isSoldier = isSoldier;
         this.soldierType = soldierType;
+        this.tools = tools;
+    }
+    public Villager(boolean bow, UpperClass boss, Tool[] tools, boolean isSoldier, String firstname, String paternalSurname, String maternalSurname, boolean gender, boolean royalBlood, boolean darknessInside, boolean banished, boolean hasEducation, float wage, float fortune, float taxes, int age, Kingdom kingdom, Occupation occupation, HealthState healthState) {
+        super(firstname, paternalSurname, maternalSurname, gender, royalBlood, darknessInside, banished, hasEducation, wage, fortune, taxes, age, kingdom, occupation, healthState,bow);
+        this.boss = boss;
+        this.isSoldier = isSoldier;
         this.tools = tools;
     }
 }
